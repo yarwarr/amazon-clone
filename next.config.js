f2eb7a11e566c02b55fcfdef6a45018267bc79d2
Mubasharyar <maw8051@rit.edu>
2023-02-22 03:10:32 -0500

    try this

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
+    loader: "akamai",
+    path: "",
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
-  }
+  },
 };
